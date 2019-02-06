b078acc4be3f16c0cd095ea248f7d8d3e506af76
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-06 12:11:46 +0000

    test: Failing feature test for user deleting own posts

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -16,4 +16,15 @@ feature "Timeline", type: :feature do
     expect(page).to have_content("created by test@test.com - user 3")
   end
 
+  scenario "User can't delete another user's post" do 
+    go_homepage
+    sign_up
+    create_post
+    click_link "Sign out"
+    sign_up2
+    click_link "posts"
+    click_link "Delete Post"
+    expect(page).to have_content("Hello, world!")
+  end
+
 end
