398663baf9928e40ba99cb6539314e378fb9f974
celine kaslin <celine.kaslin@gmail.com>
2019-02-07 12:35:33 +0000

    refactor: rubocop offenses corrected

--- spec/features/posts_display_spec.rb
+++ spec/features/posts_display_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
 require 'webhelper'
 require 'timecop'
-require 'orderly'
 
-describe "Posts" do
-  it "should have a visible date" do
+describe 'posts show the date they were posted' do
+  it 'post should have a visible date' do
     Timecop.freeze
     go_homepage
     sign_up
     create_post
-  expect(page).to have_content(Time.now.strftime('%a, %d, %b, %Y, %H:%M:%S'))
-  end
-
-  it "should display newest first" do
-    go_homepage
-    sign_up
-    create_post
-    create_second_post
-    expect('My second post, MOO!').to appear_before('Hello, world!')
+    expect(page).to have_content(Time.now.strftime('%a, %d, %b, %Y, %H:%M:%S'))
   end
 end
-
